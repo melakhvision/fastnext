aac89ccc36ea22e2738d11a81ecadbb5479d8a1f
Codekhalipha <45154979+melakhvision@users.noreply.github.com>
2022-12-04 20:36:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          This is amazing woww !!!!{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
