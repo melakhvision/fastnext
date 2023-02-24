979e20a62919cff4b9affb30a90bee5e7b024b48
Codekhalipha <45154979+melakhvision@users.noreply.github.com>
2023-02-24 04:20:49 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Comming from the pipeline wow updated awsome!!!
+          Comming from the pipeline wow updated removed !
           <code className={styles.code}>pages/index.js</code>
         </p>
 
